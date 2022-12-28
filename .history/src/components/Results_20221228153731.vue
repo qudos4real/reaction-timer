dca343d34a7d57a9e420d215936fb683da42b6dc
Qudos4real <qudos4real@yahoo.com>
2022-12-28 15:37:51 +0100

    Build prod

---
+++ .history/src/components/Results_20221228153731.vue
@@ -0,0 +1,71 @@
+<template>
+  <p>Your reaction time was <strong>{{ score }} ms </strong></p>
+  <p class="rank">{{ rank }}</p>
+  <div v-if="resultList.length" class="prevResults">
+    <p class="scores prev">Your previous scores:</p>
+    <ol reversed>
+      <li v-for="result in resultList" :key="result"> <strong>{{ result }} ms </strong></li>
+    </ol>
+    <button class="btn reset" @click="resetScores">Reset scores</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score", "resultList"],
+  data() {
+    return {
+      rank: null,
+      removeButton: false,
+    };
+  },
+  mounted() {
+    if (this.score < 100) {
+      this.rank = "Very Fast Reaction!";
+    } else if (this.score < 200) {
+      this.rank = "Fast Reaction!";
+    } else if (this.score < 300) {
+      this.rank = "Good Reaction!";
+    } else if (this.score < 400) {
+      this.rank = "Slow Reaction!";
+    } else {
+      this.rank = "Very Slow Reaction!";
+    }
+  },
+  methods: {
+    resetScores() {
+      this.$emit("resetScores");
+    },
+  },
+};
+</script>
+
+<style>
+.prevResults {
+  width: 400px;
+  margin: 0 auto;
+  margin-top: 50px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+}
+.prevResults > .reset {
+  margin-top: 20px;
+  font-size: 12px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  background-color: rgba(46, 172, 46, 0.727);
+}
+.prevResults > .reset:hover {
+  background-color: rgba(46, 172, 46, 0.9);
+}
+ul > li {
+  margin: 5px 0;
+}
+.rank, .prev {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 20px;
+  color: green;
+}
+</style>
